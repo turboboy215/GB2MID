80cf657096b83a065a50132dd764ee285fb09c82
turboboy215 <superwill@live.com>
2025-02-09 21:07:59 -0800

    Add files via upload

---
+++ GBMLIB/SHARED.C
@@ -0,0 +1,240 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+#include "SHARED.H"
+#include "MIDI.H"
+#include "MCOOKSEY.H"
+#include "TIERTEX.H"
+#define bankSize 16384
+
+int foundTable = 0;
+int curInst = 0;
+int curVol = 100;
+int curBank = 0;
+int multiBanks = 0;
+
+/*Convert little-endian pointer to big-endian*/
+unsigned short ReadLE16(unsigned char* Data)
+{
+	return (Data[0] << 0) | (Data[1] << 8);
+}
+
+/*Store big-endian pointer*/
+unsigned short ReadBE16(unsigned char* Data)
+{
+	return (Data[0] << 8) | (Data[1] << 0);
+}
+
+void Write8B(unsigned char* buffer, unsigned int value)
+{
+	buffer[0x00] = value;
+}
+
+void WriteBE32(unsigned char* buffer, unsigned long value)
+{
+	buffer[0x00] = (value & 0xFF000000) >> 24;
+	buffer[0x01] = (value & 0x00FF0000) >> 16;
+	buffer[0x02] = (value & 0x0000FF00) >> 8;
+	buffer[0x03] = (value & 0x000000FF) >> 0;
+
+	return;
+}
+
+void WriteBE24(unsigned char* buffer, unsigned long value)
+{
+	buffer[0x00] = (value & 0xFF0000) >> 16;
+	buffer[0x01] = (value & 0x00FF00) >> 8;
+	buffer[0x02] = (value & 0x0000FF) >> 0;
+
+	return;
+}
+
+void WriteBE16(unsigned char* buffer, unsigned int value)
+{
+	buffer[0x00] = (value & 0xFF00) >> 8;
+	buffer[0x01] = (value & 0x00FF) >> 0;
+
+	return;
+}
+
+void WriteLE16(unsigned char* buffer, unsigned int value)
+{
+	buffer[0x00] = (value & 0x00FF) >> 0;
+	buffer[0x01] = (value & 0xFF00) >> 8;
+
+	return;
+}
+
+void WriteLE24(unsigned char* buffer, unsigned long value)
+{
+	buffer[0x00] = (value & 0x0000FF) >> 0;
+	buffer[0x01] = (value & 0x00FF00) >> 8;
+	buffer[0x02] = (value & 0xFF0000) >> 16;
+
+	return;
+}
+
+void WriteLE32(unsigned char* buffer, unsigned long value)
+{
+	buffer[0x00] = (value & 0x000000FF) >> 0;
+	buffer[0x01] = (value & 0x0000FF00) >> 8;
+	buffer[0x02] = (value & 0x00FF0000) >> 16;
+	buffer[0x03] = (value & 0xFF000000) >> 24;
+
+	return;
+}
+
+unsigned int WriteNoteEvent(unsigned char* buffer, unsigned int pos, unsigned int note, int length, int delay, int firstNote, int curChan, int inst)
+{
+	int deltaValue;
+	deltaValue = WriteDeltaTime(buffer, pos, delay);
+	pos += deltaValue;
+
+	if (firstNote == 1)
+	{
+		if (curChan != 3)
+		{
+			Write8B(&buffer[pos], 0xC0 | curChan);
+		}
+		else
+		{
+			Write8B(&buffer[pos], 0xC9);
+		}
+
+		Write8B(&buffer[pos + 1], inst);
+		Write8B(&buffer[pos + 2], 0);
+
+		if (curChan != 3)
+		{
+			Write8B(&buffer[pos + 3], 0x90 | curChan);
+		}
+		else
+		{
+			Write8B(&buffer[pos + 3], 0x99);
+		}
+
+		pos += 4;
+	}
+
+	Write8B(&buffer[pos], note);
+	pos++;
+	Write8B(&buffer[pos], 100);
+	pos++;
+
+	deltaValue = WriteDeltaTime(buffer, pos, length);
+	pos += deltaValue;
+
+	Write8B(&buffer[pos], note);
+	pos++;
+	Write8B(&buffer[pos], 0);
+	pos++;
+
+	return pos;
+
+}
+
+int WriteDeltaTime(unsigned char* buffer, unsigned int pos, unsigned int value)
+{
+	unsigned char valSize;
+	unsigned char* valData;
+	unsigned int tempLen;
+	unsigned int curPos;
+
+	valSize = 0;
+	tempLen = value;
+
+	while (tempLen != 0)
+	{
+		tempLen >>= 7;
+		valSize++;
+	}
+
+	valData = &buffer[pos];
+	curPos = valSize;
+	tempLen = value;
+
+	while (tempLen != 0)
+	{
+		curPos--;
+		valData[curPos] = 128 | (tempLen & 127);
+		tempLen >>= 7;
+	}
+
+	valData[valSize - 1] &= 127;
+
+	pos += valSize;
+
+	if (value == 0)
+	{
+		valSize = 1;
+	}
+	return valSize;
+}
+
+unsigned int WriteNoteEventOn(unsigned char* buffer, unsigned int pos, unsigned int note, int length, int delay, int firstNote, int curChan, int inst)
+{
+	int deltaValue;
+	deltaValue = WriteDeltaTime(buffer, pos, delay);
+	pos += deltaValue;
+
+	if (firstNote == 1)
+	{
+		Write8B(&buffer[pos], 0xC0 | curChan);
+
+		Write8B(&buffer[pos + 1], inst);
+		Write8B(&buffer[pos + 2], 0);
+
+		Write8B(&buffer[pos + 3], 0x90 | curChan);
+
+		pos += 4;
+	}
+
+	Write8B(&buffer[pos], note);
+	pos++;
+	Write8B(&buffer[pos], curVol);
+	pos++;
+
+	return pos;
+
+}
+
+unsigned int WriteNoteEventOff(unsigned char* buffer, unsigned int pos, unsigned int note, int length, int delay, int firstNote, int curChan, int inst)
+{
+	int deltaValue;
+
+	deltaValue = WriteDeltaTime(buffer, pos, delay);
+	pos += deltaValue;
+
+	Write8B(&buffer[pos], note);
+	pos++;
+	Write8B(&buffer[pos], 0);
+	pos++;
+	return pos;
+}
+
+void gb2MID(FILE* rom, long banks[50], int numBanks, long format, char parameters[4][50])
+{
+	if (numBanks > 1)
+	{
+		multiBanks = 1;
+	}
+	for (curBank == 0; curBank < numBanks; curBank++)
+	{
+		switch (format)
+		{
+		case MIDI:
+			MIDProc();
+			break;
+		case Mark_Cooksey:
+			MCProc(banks[curBank]);
+			break;
+		case Tiertex:
+			TTProc(banks[curBank], parameters);
+			break;
+		default:
+			MIDProc();
+			break;
+		}
+	}
+}
